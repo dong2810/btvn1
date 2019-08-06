05f5cb1993b87621ce63ea2801435153cd37a90b
dong2810 <dongvip99@gmail.com>
2019-08-06 14:42:16 +0700

    out

---
+++ src/simple/Main.java
@@ -0,0 +1,9 @@
+package simple;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("my name is Dong");
+    }
+}
